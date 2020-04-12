9679358b85088c01149542c9e6c0f428b9043478
Andy Horn <andyjhorn@gmail.com>
2020-04-12 18:05:47 -0500

    added route contracts and endpoints for logs controller

--- api/Contracts/Routes.cs
+++ api/Contracts/Routes.cs
@@ -6,6 +6,12 @@ namespace API.Contracts
         public static class Logs
         {
             public const string Get = "";
+            public const string GetById = "/{id}";
+            public const string GetForUser = "/user/{id}";
+            public const string Update = "/{id}";
+            public const string Delete = "/{id}";
+            public const string Save = "/save";
+            public const string Upload = "/";
         }
     }
 }
--- api/Controllers/LogsController.cs
+++ api/Controllers/LogsController.cs
@@ -1,3 +1,4 @@
+using System;
 using API.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,48 @@ namespace API.Controllers
         public IActionResult Get()
         {
             return Ok("Hello world!");
+        }
+
+        [HttpGet]
+        [Route(Routes.Logs.GetById)]
+        public IActionResult GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route(Routes.Logs.GetForUser)]
+        public IActionResult GetForUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route(Routes.Logs.Update)]
+        public IActionResult Update(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route(Routes.Logs.Delete)]
+        public IActionResult Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route(Routes.Logs.Save)]
+        public IActionResult Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route(Routes.Logs.Upload)]
+        public IActionResult Upload()
+        {
+            throw new NotImplementedException();
         }
     }
 }
