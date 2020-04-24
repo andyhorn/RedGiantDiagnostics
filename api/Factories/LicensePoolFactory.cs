c74c286185a6afa45aada534b74818f229e5d77d
Andy Horn <andyjhorn@gmail.com>
2020-04-24 14:25:42 -0500

    license pool factory passing all unit tests

--- api/Factories/LicensePoolFactory.cs
+++ api/Factories/LicensePoolFactory.cs
@@ -24,6 +24,11 @@ namespace API.Factories
 
         public LicensePool Parse(string[] data)
         {
+            if (data == null || data.Length == 0)
+            {
+                return null;
+            }
+
             var pool = New;
 
             pool.Product = GetProductName(data);
@@ -38,14 +43,26 @@ namespace API.Factories
         {
             var collection = new List<string>();
             
-            var checkoutLines = 
-                _utilities.GetLinesBetween("Usage for pool", null, data)
-                .Where(x => !x.Contains("======="));
+            // Get the usage sections
+            var checkoutLines = _utilities.GetLinesBetween("Usage for pool", null, data);
 
+            // Validate the data
+            if (checkoutLines == null || checkoutLines.Length == 0)
+            {
+                return collection;
+            }
+
+            // Filter out the separators
+            checkoutLines = checkoutLines.Where(x => !x.Contains("======")).ToArray();
+
+            // Parse the checkout data from each line
             foreach (var checkoutLine in checkoutLines)
             {
                 var checkout = GetCheckout(checkoutLine);
-                collection.Add(checkout);
+
+                // If valid data was returned, add it to the collection
+                if (!string.IsNullOrWhiteSpace(checkout))
+                    collection.Add(checkout);
             }
 
             return collection;
@@ -53,10 +70,23 @@ namespace API.Factories
 
         private string GetCheckout(string data)
         {
+            var user = string.Empty;
+            var host = string.Empty;
+            var time = string.Empty;
+
+            // Split the string
             var columns = data.Split(":");
-            var user = columns[0];
-            var host = columns[1];
-            var time = columns[8];
+
+            // Check the length of the array and 
+            // extract the available data
+            if (columns.Length > 0)
+                user = columns[0];
+            
+            if (columns.Length > 1)
+                host = columns[1];
+
+            if (columns.Length > 8)
+                time = columns[8];
 
             var value = $"{user}:{host} - {time}";
             return value;
@@ -64,25 +94,55 @@ namespace API.Factories
 
         private int GetInUseSeats(string[] data)
         {
-            var inUse = _utilities.GetLineValue("Pool", 7, data);
-            inUse = inUse.Substring("inuse:".Length);
+            int value = 0;
 
-            var inUseSeats = int.Parse(inUse);
-            return inUseSeats;
+            // Get the desired string
+            var inUse = _utilities.GetLineValue("Pool", 7, data);
+
+            // Validate the data
+            if (string.IsNullOrWhiteSpace(inUse))
+            {
+                return value;
+            }
+
+            // If the header exists, remove it
+            if (inUse.Contains("inuse:"))
+            {
+                inUse = inUse.Substring("inuse:".Length);
+            }
+
+            int.TryParse(inUse, out value);
+            return value;
         }
 
         private int GetTotalSeats(string[] data)
         {
-            var total = _utilities.GetLineValue("Pool", 6, data);
-            total = total.Substring("Soft:".Length);
+            int value = 0;
 
-            var totalSeats = int.Parse(total);
-            return totalSeats;
+            // Get the desired string
+            var total = _utilities.GetLineValue("Pool", 6, data);
+
+            // Validate the data
+            if (string.IsNullOrWhiteSpace(total))
+            {
+                return value;
+            }
+
+            // If the header exists, remove it
+            if (total.Contains("Soft:"))
+            {
+                total = total.Substring("Soft:".Length);
+            }
+
+            // Try to parse the integer
+            int.TryParse(total, out value);
+
+            return value;
         }
 
         private string GetProductName(string[] data)
         {
-            var name = _utilities.GetLineValue("Pool", 2, data);
+            var name = _utilities.GetLineValue("Pool", 2, data) ?? string.Empty;
             return name;
         }
     }
