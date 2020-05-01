3e5596eee691440f22c9d295863cdac92d20a699
Andy Horn <andyjhorn@gmail.com>
2020-05-01 16:53:26 -0500

    removed attributes

--- api/Extensions/Attributes.cs
+++ api/Extensions/Attributes.cs
@@ -4,26 +4,5 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace API
 {
-    public class AdminOrCurrentUserAttribute : ActionFilterAttribute
-    {
-        private IIdentityService _identityService;
-        
-        public AdminOrCurrentUserAttribute(IIdentityService identity)
-        {
-            _identityService = identity;
-        }
-
-        public async override void OnActionExecuting(ActionExecutingContext context)
-        {
-            var currentUser = await context.HttpContext.CurrentUser(_identityService);
-            var requestUserId = (string)context.RouteData.Values["id"];
-
-            var roles = await _identityService.GetUserRolesAsync(currentUser);
-
-            if (currentUser.Id != requestUserId && !roles.Contains(Contracts.Roles.Admin))
-            {
-                context.HttpContext.Response.StatusCode = 403;
-            }
-        }
-    }
+    
 }
