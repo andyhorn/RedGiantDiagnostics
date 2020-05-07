464ea3c329f674ef139d0f4d100acbe821e5fa7a
Andy Horn <andyjhorn@gmail.com>
2020-05-06 19:21:54 -0500

    file form in place

---
+++ web/src/components/Home/UploadForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <b-form @submit:prevent="onSubmit" @reset="onReset">
+      <b-form-group id="file-input-group" description="Select the log file to analyze">
+        <h2>Upload a file</h2>
+        <b-form-file
+          v-model="file"
+          :state="Boolean(file)"
+          placeholder="Select a file..."
+          no-drop
+          accept=".txt"
+          :file-name-formatter="fileNameFormatter"
+        />
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UploadForm",
+  data() {
+    return {
+      file: null
+    };
+  },
+  methods: {
+    fileNameFormatter() {
+      if (this.file) {
+        return this.file.name;
+      } else {
+        return null;
+      }
+    },
+    onSubmit() {
+
+    },
+    onReset() {
+        
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container mt-5">
+    <UploadForm />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import UploadForm from "@/components/Home/UploadForm";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    UploadForm
   }
 };
 </script>
