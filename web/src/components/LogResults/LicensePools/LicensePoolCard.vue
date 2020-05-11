dae0226de2505a7fb5bdc88f3123149abeaca57a
Andy Horn <andyjhorn@gmail.com>
2020-05-11 14:41:01 -0500

    updated formatting on the license pool tables and improved logic to determine whether or not to display the pool

--- web/src/components/LogResults/LicensePools/LicensePoolCard.vue
+++ web/src/components/LogResults/LicensePools/LicensePoolCard.vue
@@ -1,8 +1,8 @@
 <template>
     <b-card>
         <b-card-body>
-            <b-card-title>ISV Server: <span class="name">{{ serverName }}</span></b-card-title>
-            <LicensePoolTable v-if="licensePools.length > 0" :pools="licensePools" />
+            <p class="card-title">ISV Server: {{ serverName }}</p>
+            <LicensePoolTable v-if="shouldDisplay(licensePools)" :pools="licensePools" />
             <div v-else>
                 <p>No licenses in use</p>
             </div>
@@ -22,6 +22,21 @@ export default {
     methods: {
         title() {
             return `ISV Server: <span class="name">${this.serverName}</span>`;
+        },
+        shouldDisplay(pools) {
+            // If no pools are in the list, do not display
+            if (pools.length === 0) return false;
+            
+            // If none of the pools have any licenses in use, do not display
+            let display = false;
+            for (let pool of pools) {
+                if (pool.inUse > 0) {
+                    display = true;
+                    break;
+                }
+            }
+
+            return display;
         }
     }
 }
@@ -30,9 +45,5 @@ export default {
 <style scoped>
 .card-title {
     font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-}
-.name {
-    font-family: 'Courier New', Courier, monospace;
-    font-weight: 500;
 }
 </style>
