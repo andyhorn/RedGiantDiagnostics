ac133a9fb01bccd9808c003abe7545d4b28cfc0c
Andy Horn <andyjhorn@gmail.com>
2020-05-12 22:05:43 -0500

    connected login form to data members

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,15 +1,14 @@
 <template>
 <div>
-    <b-dropdown right id="dropdown-form" text="Login">
+    <b-dropdown right id="dropdown-form" text="Login" ref="loginDropdown">
         <b-dropdown-form @submit.prevent="onLogin">
             <b-form-group label="Email" label-for="email-address-input" @submit.stop.prevent>
-                <b-form-input size="sm" id="email-address-input" type="email" placeholder="email@domain.com" />
+                <b-form-input size="sm" v-model="email" id="email-address-input" type="email" placeholder="email@domain.com" />
             </b-form-group>
             <b-form-group label="Password" label-for="password-input">
-                <b-form-input size="sm" id="password-input" type="password" placeholder="password" />
+                <b-form-input size="sm" v-model="password" id="password-input" type="password" placeholder="password" />
             </b-form-group>
-
-            <b-form-checkbox class="mb-3">Remember me</b-form-checkbox>
+            <b-form-checkbox v-model="rememberMe" class="mb-3">Remember me</b-form-checkbox>
             <b-button class="float-right" variant="primary" type="submit">Login</b-button>
         </b-dropdown-form>
     </b-dropdown>
@@ -21,14 +20,17 @@ export default {
     name: 'Login',
     data() {
         return {
-            email: null,
-            password: null,
+            email: "",
+            password: "",
             rememberMe: false
         }
     },
     methods: {
         onLogin() {
-
+            this.$refs.loginDropdown.hide();
+            console.log(this.email)
+            console.log(this.password)
+            console.log(this.rememberMe)
         }
     }
 }
