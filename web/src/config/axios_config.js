318e37f90be5c5dc7de6d7b633a96ec9f58bc2ee
Andy Horn <andyjhorn@gmail.com>
2020-06-08 07:42:09 -0500

    trying to configure axios and nginx for production

--- web/src/config/axios_config.js
+++ web/src/config/axios_config.js
@@ -5,10 +5,11 @@ const baseURL = process.env.VUE_APP_BACKEND_URL || 'http://localhost:5000/api/v2
 const options = {
   headers: {
     'Content-Type': 'application/json'
-  }
+  },
+  baseURL: baseURL
 }
 
-if (baseURL) options.baseURL = baseURL;
+// if (baseURL) options.baseURL = baseURL;
 
 const http = axios.create(options);
 
